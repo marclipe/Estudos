6f12a2610ceb388ea820185f10535fe4efe54e52
marcos.lima <marcos.lima@scaleup.com.br>
2022-11-25 13:38:16 -0300

    Propriedades opcionais (TS)

--- typescript/src/main.ts
+++ typescript/src/main.ts
@@ -1,17 +1,25 @@
-type UF = 'PE' | 'SP' | 'MG'
+// type UF = 'PE' | 'SP' | 'MG';
 
 //Mostrar a cidade do usuário
 interface User {
-    name: string, 
-    address: {
-        city: string, 
-        UF: UF
-    }
-    sayHello: () => void
+  name: string;
+  address?: {
+    city: string;
+    UF: string;
+  };
+  //   sayHello: () => void;
 }
 
 function showCity(user: User) {
-    user.sayHello = () => console.log('cataline')
-    user.address.UF = 'SP'
-    return user.address.city
+  return user.address?.city;
 }
+
+const response = showCity({
+  name: 'MarcLipe',
+  address: {
+    city: 'Barras',
+    UF: 'PI'
+  }
+});
+
+console.log(response);
