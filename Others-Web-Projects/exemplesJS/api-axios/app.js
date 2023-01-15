c0c5c51aa94862c3c208fa73d41fb80f72812da7
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-01-15 00:21:09 -0300

    Api axios working

--- Others-Web-Projects/exemplesJS/api-axios/app.js
+++ Others-Web-Projects/exemplesJS/api-axios/app.js
@@ -1,11 +1,17 @@
 var button = document.querySelector("#app"); 
 
+if(button){
 button.addEventListener('click', function() {
-    axios.get('https://api.github.com/users')
-    .then(function() {
-        console.log('deu certo')
+    axios
+    .get('https://api.github.com/users')
+    .then(function(response) {
+        console.log(response.data[0].login)
     })
-    .catch(() => {
-        console.log('deu ruim')
+    .catch((error) => {
+        console.log(error)
+    })
+    .finally  (() => {
+        console.log('finalizou')
     })
 })
+}
